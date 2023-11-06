a6a35045fe418d21ae08e033b6f7037679805e69
Shane Alam <shanealam6123@gmail.com>
2023-11-06 19:49:01 +0530

    done final

--- app.py
+++ app.py
@@ -1,6 +1,41 @@
-from flask import Flask
+from flask import Flask, request, send_file, jsonify
+import rembg
+import io
+from PIL import Image
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        if 'image' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+        
+        file = request.files['image']
+        
+        if file.filename == '':
+            return jsonify({'error': 'No selected file'}), 400
+        
+        image_data = file.read()
+        
+        # Remove background using rembg library
+        output_data = rembg.remove(image_data)
+        
+        # Create a PIL Image from the output data
+        output_image = Image.open(io.BytesIO(output_data))
+        
+        # Save the output image (optional)
+        # output_image.save('output.png')
+        
+        # Send the image as a response
+        output_buffer = io.BytesIO()
+        output_image.save(output_buffer, format='PNG')
+        output_buffer.seek(0)
+        
+        return send_file(output_buffer, mimetype='image/png'), 200
+    
+    except Exception as e:
+        return jsonify({'error': f'An error occurred: {str(e)}'}), 500
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
